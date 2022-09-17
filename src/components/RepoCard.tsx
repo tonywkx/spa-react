5525811730615498dbaf85416832b643aa10907b
tonywkx <tonywk@ya.ru>
2022-09-17 18:47:28 +0400

    feat: finish spa with open github api

---
+++ src/components/RepoCard.tsx
@@ -0,0 +1,51 @@
+
+import React, { useState } from 'react'
+import { useActions } from '../hooks/actions'
+import { useAppSelector } from '../hooks/redux'
+import { IRepo } from '../models/model'
+
+export function RepoCard({repo}: {repo: IRepo}) {
+
+    const {addFavorite, removeFavorite} = useActions()
+
+    const {favourites} = useAppSelector(state => state.github)
+
+    const [isFav, setIsFav] = useState(favourites.includes(repo.html_url))
+
+    const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+
+        addFavorite(repo.html_url)
+        setIsFav(true)
+    }
+
+    const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+
+        removeFavorite(repo.html_url)
+        setIsFav(false)
+    }
+
+
+  return (
+    <div className='border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all'>
+        <a href={repo.html_url} target="_blank" rel="noreferrer">
+        <h2 className='text-lg font-bold'> {repo.full_name}</h2>
+        <p className='text-sm'>
+            Forks: <span className='font-bold mr-2'>{repo.forks}</span>
+            Language: <span className='font-bold mr-2'>{repo.language}</span>
+            Watchers: <span className='font-bold'>{repo.watchers}</span>
+        </p>
+        <p className='text-sm font-thin'>{repo?.description}</p>
+
+        { !isFav && <button className='py-2 px-4 bg-yellow-400 rounded mr-2 hover:shadow-md transition-all'
+        onClick={addToFavourite}
+        >Add</button>}
+
+        { isFav && <button className='py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all'
+        onClick={removeFromFavourite}
+        >Remove</button>}
+        </a>
+    </div>
+  )
+}
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import { IUser, ServerResponse } from '../../models/model';
+import { IRepo, IUser, ServerResponse } from '../../models/model';
 
 export const githubApi = createApi({
     reducerPath: 'github/api',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://api.github.com/'
     }),
+    refetchOnFocus: true,
     endpoints: build => ({
         searchUsers: build.query<IUser[], string>({
             query: (search: string) => ({
@@ -18,8 +19,13 @@ export const githubApi = createApi({
                 }                
             }),
             transformResponse: (response: ServerResponse<IUser>) => response.items
+        }),
+        getUserRepos: build.query<IRepo[], string>({
+            query: (username: string) => ({
+                url: `users/${username}/repos`
+            })
         })
     })
 })
 
-export const {useSearchUsersQuery} =  githubApi;
+export const {useSearchUsersQuery, useLazyGetUserReposQuery} =  githubApi;
